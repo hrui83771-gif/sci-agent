edc5b1a6276c3f5c24b8db0c347ea893ddce655c
hrui83771-gif <hrui83771@gmail.com>
2026-04-03 08:18:46 +0800

    Delete src directory

--- src/prompts/modes/index.js
+++
@@ -1,5 +0,0 @@
-import { quickAssistantPrompt } from './quickAssistant.js'
-
-export function getQuickAssistantPrompt(locale = 'zh') {
-  return quickAssistantPrompt.locales[locale] || quickAssistantPrompt.locales.zh
-}
